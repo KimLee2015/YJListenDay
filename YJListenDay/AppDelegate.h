8ae5b6d8e2ef728316c1ac2eccffc2eb07f2e0e3
Lee <liyaojianaikaba@126.com>
2015-02-12 20:37:15 +0800

    Initial Commit

---
+++ YJListenDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJListenDay
+//
+//  Created by Lee on 15/2/12.
+//  Copyright (c) 2015年 Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
